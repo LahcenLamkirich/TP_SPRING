4e83b3a2786ab4c911f1956a2fab680bebde7383
LahcenLamkirich <lahcen.lamkirich@gmail.com>
2022-05-03 22:12:36 +0000

    commitService

--- TPs/TP4/src/main/java/com/lamkirich/hospital/Service/HospitalService.java
+++ TPs/TP4/src/main/java/com/lamkirich/hospital/Service/HospitalService.java
@@ -1,5 +1,13 @@
 package com.lamkirich.hospital.Service;
 
+import com.lamkirich.hospital.Entities.Consultation;
+import com.lamkirich.hospital.Entities.Medecin;
+import com.lamkirich.hospital.Entities.Patient;
+import com.lamkirich.hospital.Entities.RendezVous;
+
 public interface HospitalService {
-    
+    Patient savePatient(Patient patient);
+    Medecin saveMedecin(Medecin medecin);
+    RendezVous saveRendezVous(RendezVous rendezVous);
+    Consultation saveConsultation(Consultation consultation);
 }
---
+++ TPs/TP4/src/main/java/com/lamkirich/hospital/Service/HospitalServiceImp.java
@@ -0,0 +1,48 @@
+package com.lamkirich.hospital.Service;
+
+import com.lamkirich.hospital.Entities.Consultation;
+import com.lamkirich.hospital.Entities.Medecin;
+import com.lamkirich.hospital.Entities.Patient;
+import com.lamkirich.hospital.Entities.RendezVous;
+import com.lamkirich.hospital.Repositories.ConsultationRepository;
+import com.lamkirich.hospital.Repositories.MedecinRepository;
+import com.lamkirich.hospital.Repositories.PatientRepository;
+import com.lamkirich.hospital.Repositories.RendezVousRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+
+@Service
+@Transactional
+public class HospitalServiceImp implements HospitalService{
+    @Autowired
+    PatientRepository patientRepository ;
+    @Autowired
+    MedecinRepository medecinRepository ;
+    @Autowired
+    RendezVousRepository rendezVousRepository ;
+    @Autowired
+    ConsultationRepository consultationRepository ;
+
+
+    @Override
+    public Patient savePatient(Patient patient) {
+        return patientRepository.save(patient);
+    }
+
+    @Override
+    public Medecin saveMedecin(Medecin medecin) {
+        return medecinRepository.save(medecin);
+    }
+
+    @Override
+    public RendezVous saveRendezVous(RendezVous rendezVous) {
+        return rendezVousRepository.save(rendezVous);
+    }
+
+    @Override
+    public Consultation saveConsultation(Consultation consultation) {
+        return consultationRepository.save(consultation);
+    }
+}
